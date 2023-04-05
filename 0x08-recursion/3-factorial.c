c223469498949f9daed969e70f34ff72c24f94cb
Hamiltonmcrop <hamiltonmcrop1@gmail.com>
2023-04-05 14:30:41 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+/**
+ * factorial - find  the factorial of *n
+ *
+ * @n: positive integer
+ *
+ * Return: @n factorial
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
