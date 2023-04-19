34d253ddcc1ea49f35282e2f2893b8ece02cddc7
Hamiltonmcrop <hamiltonmcrop1@gmail.com>
2023-04-19 16:59:27 -0700

    function_pointers.h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Print name with pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
